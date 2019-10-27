6bea4fc8225267fec21467ffe990395a08d4d23e
Viktor Papara <eucalyptov@gmail.com>
2019-10-27 21:10:49 +0100

    Refactor training logger into proper classes

--- training-repetition-logger/training-logger.py
+++ training-repetition-logger/training-logger.py
@@ -3,52 +3,95 @@ This logger waits for you to input the number of repetitions you just did.
 It then logs them for you with the current timestamp.
 """
 import datetime
-
-logstring = ""
-print(logstring)
-
-workout = [0]
+from dataclasses import dataclass
+from typing import List
 
 
-def change_last_number(correction):
-    # change the last number to: correction
-    print("changing number to:", correction)
-    pass
+@dataclass
+class Entry:
+    """
+    This dataclass stores a timestamp entry with timestamp text and number of repetitions.
+    """
+    timestamp: str
+    reps: int
 
 
-while True:
-    inputline = input(f"Please insert number of repetitions (default: {workout[-1]}, or '[h]elp'):")
-    if inputline.lower().startswith("q"):
-        print("Here's your workout again:\n"
-              + logstring
-              + "Total reps: "
-              + str(sum(workout)))
-        break
-    elif inputline == "":
-        inputline = workout[-1]
-    elif inputline.lower().startswith("h"):
+class Workout:
+    """
+    Main class with data and logic (backend)
+    """
+
+    def __init__(self):
+        self.entries: List[Entry] = []
+
+    def change_last_number(correction):
+        # change the last number to: correction
+        print("changing number to:", correction)
+        pass
+
+    def add_entry(self, reps: int):
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "; "
+        self.entries.append(Entry(timestamp, reps))
+
+    def __str__(self):
+        output = "Your workout:\n"
+        for entry in self.entries:
+            output += entry.timestamp + str(entry.reps) + "\n"
+        output += "Total reps: " + str(self.total())
+        return output
+
+    def total(self) -> int:
+        return sum([entry.reps for entry in self.entries])
+
+
+class Workouts:
+    """
+    Helper class that does input-output
+    """
+
+    @classmethod
+    def run_in_command_line(cls):
+        workout = Workout()
+        while True:
+            last_reps = workout.entries[-1].reps \
+                if workout.entries != [] \
+                else 0
+            inputline = input("Please insert number of repetitions"
+                              f"(default: {last_reps}, or '[h]elp'):")
+            if inputline == "":
+                workout.add_entry(last_reps)
+                print(workout)
+            elif inputline.lower().startswith("h"):
+                Workouts.print_help()
+            elif inputline.lower().startswith("q"):
+                print(workout)
+                break
+            elif inputline.lower().startswith("p"):
+                print(workout)
+            elif inputline.lower().startswith("a"):
+                # Get correct number
+                correct = int(input("Please give the new number:"))
+                change_last_number(correct)
+                continue
+            else:
+                try:
+                    reps = int(inputline)
+                except ValueError as verror:
+                    print("Could not convert input into a number.")
+                    print(verror)
+                    print("Please try again.")
+                    continue
+                workout.add_entry(reps)
+                print(workout)
+
+    @classmethod
+    def print_help(cls):
         print("You have the following commands:\n"
               + "[h]elp: this help message\n"
               + "[q]uit: print workout and quit\n"
-              + "[a]mend: amend last number without changing the timestamp")
-        continue
-    elif inputline.lower().startswith("a"):
-        # Get correct number
-        correct = int(input("Please give the new number:"))
-        change_last_number(correct)
-        continue
+              + "[a]mend: amend last number without changing the timestamp\n"
+              + "[p]rint: print current workout\n")
 
-    try:
-        reps = int(inputline)
-    except ValueError as verror:
-        print("Could not convert input into a number.")
-        print(verror)
-        print("Please try again.")
-        continue
 
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "; "
-    workout.append(reps)
-    logstring += timestamp + str(reps) + "\n"
-    print("your workout so far:\n"
-          + logstring + "Total reps: "
-          + str(sum(workout)))
+if __name__ == '__main__':
+    Workouts.run_in_command_line()
