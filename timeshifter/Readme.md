8c36ee0b08a4ca90cf615bdda62d53b56a03af67
Viktor Papara <developer@eucalypto.net>
2021-01-26 17:59:31 +0100

    Update Readme and clean code a bit #10

--- timeshifter/Readme.md
+++ timeshifter/Readme.md
@@ -1,3 +1,11 @@
 # Timeshifter
 
 This command line app shifts a bunch of timestamps by a constant amount.
+
+## Usage
+
+```bash
+$ java -jar timeshifter-0.1.0.jar 00:01:13 "00:02:23,01:01:00,01:01:13,00:01:13"
+
+00:01:10,00:59:47,01:00:00,00:00:00
+```
--- timeshifter/src/main/kotlin/controller/Controller.kt
+++ timeshifter/src/main/kotlin/controller/Controller.kt
@@ -7,10 +7,10 @@ import kotlin.streams.toList
 class Controller {
     fun pipeArgs(args: Array<String>) {
         // java -jar timeshifter.jar 00:00:13 "00:00:34,00:01:00,01:01:03"
-        subtractTime(args[0], args[1])
+        subtractAndPrintTimeStamps(args[0], args[1])
     }
 
-    private fun subtractTime(subtractedAmountInput: String, timeStampsInput: String) {
+    private fun subtractAndPrintTimeStamps(subtractedAmountInput: String, timeStampsInput: String) {
         val timeStamps = TimeStamp.getListOf(timeStampsInput)
         val subtractedTimeStamps = timeStamps
             .stream()
